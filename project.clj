7281b1d7b7bcf209705da0c9e83b2e027a07ad9b
Tony Edgin <tedgin@iplantcollaborative.org>
2012-09-17 15:28:49 -0700

    Added descriptors to lein project

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject org.iplantc/boxy "0.0.1-SNAPSHOT"
+  :description "a mock of an iRODS repository to be used with clj-jargon"
+  :license {:url "file://LICENSE"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.irods.jargon/jargon-core "3.2.0-SNAPSHOT"]]
   :repositories {"renci.repository"
