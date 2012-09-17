2b41feed62f50e5729a9de4682c7ad630011cbc2
Tony Edgin <tedgin@iplantcollaborative.org>
2012-09-17 15:32:57 -0700

    Added project url to lein project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.iplantc/boxy "0.0.1-SNAPSHOT"
   :description "a mock of an iRODS repository to be used with clj-jargon"
+  :url "http://github.com/iPlantCollaborativeOpenSource/boxy"
   :license {:url "file://LICENSE"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.irods.jargon/jargon-core "3.2.0-SNAPSHOT"]]
