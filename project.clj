775902b7d4cda69d14daf166e5072464d5c6f3c1
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 14:51:51 -0700

    upped dependencies and created version 0.1.2-SNAPSHOT
    clojure -> 1.5.0
    clojure-commons -> 1.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject org.iplantc/boxy "0.1.1-SNAPSHOT"
+(defproject org.iplantc/boxy "0.1.2-SNAPSHOT"
   :description  "a mock of an iRODS repository to be used with clj-jargon"
   :url          "http://github.com/iPlantCollaborativeOpenSource/boxy"
   :license      {:url "file://LICENSE"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.irods.jargon/jargon-core "3.2.0"]
                  [slingshot "0.10.3"]
-                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]]
+                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/",
                  
                  "renci.repository"
-                 "http://ci-dev.renci.org/nexus/content/repositories/releases/"})
+                 "http://ci-dev.renci.org/nexus/content/repositories/releases/"
+                 
+                 "renci.repository.snapshots"
+                 "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"})
