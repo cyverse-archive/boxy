7919cfa9c76d811a2b3f343c0037116a25674f2c
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-04 10:52:33 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,23 @@
 (defproject org.iplantc/boxy "0.1.2-SNAPSHOT"
   :description  "a mock of an iRODS repository to be used with clj-jargon"
-  :url          "http://github.com/iPlantCollaborativeOpenSource/boxy"
-  :license      {:url "file://LICENSE"}
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :url "http://github.com/iPlantCollaborativeOpenSource/boxy"
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/kameleon.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.irods.jargon/jargon-core "3.3.0"]
                  [slingshot "0.10.3"]
                  [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]]
-  :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/",
-                 
-                 "renci.repository"
-                 "http://ci-dev.renci.org/nexus/content/repositories/releases/"
-                 
-                 "renci.repository.snapshots"
-                 "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]
+                 ["renci.repository.releases"
+                  {:url "http://ci-dev.renci.org/nexus/content/repositories/releases/"}]
+                 ["renci.repository.snapshots"
+                  {:url "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"}]])
